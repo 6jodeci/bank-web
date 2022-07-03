08d0c78e94de4b9df21d6a64a3f51439cdb872af
6jodeci <egor0263s@gmail.com>
2022-07-03 17:32:43 +0500

    edits

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5433/bank_db?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
