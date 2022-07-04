d8f218ab3c84310132c0ada735f66cae9799fb41
6jodeci <egor0263s@gmail.com>
2022-07-04 14:50:10 +0500

    tests fixed

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "accounts" (
-  "id" bigint PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
@@ -7,18 +7,18 @@ CREATE TABLE "accounts" (
 );
 
 CREATE TABLE "entries" (
-  "id" bigserial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-  "id" bigserial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "from_account_id" bigint NOT NULL,
   "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
-  "created_at" timestamp DEFAULT (now()) NOT NULL
+  "created_at" timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "accounts" ("owner");
