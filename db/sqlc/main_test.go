5f9c02a77bc5eb20d699be419958e3b1160f1ad1
6jodeci <egor0263s@gmail.com>
2022-07-04 18:56:05 +0500

    store&store_test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,15 +15,17 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 // run --- go test / unit test app doesn't work..
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
