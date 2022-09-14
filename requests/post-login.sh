db6340abfc6f8139ff6a2d90bc27c9ed55d847fe
Jodeci <egor0263s@gmail.com>
2022-09-15 00:15:27 +0500

    Implement login user API that returns PASETO or JWT access token

---
+++ requests/post-login.sh
@@ -0,0 +1,6 @@
+curl --location --request POST 'http://localhost:8080/users/login' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "6jodeci",
+    "password": "secret"
+}'
