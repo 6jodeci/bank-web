344f92f9980ce6585db96340180102079817602d
6jodeci <egor0263s@gmail.com>
2022-09-30 17:25:48 +0500

    run a golang gRPC server and call its API

--- Makefile
+++ Makefile
@@ -37,4 +37,7 @@ proto:
     --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
     proto/*.proto
 
-.PHONY: postgres createdb dropdb migrateup migratedown migrateup1 migratedown1 sqlc server mock proto
+evans:
+	./evans --host localhost --port 9090 -r repl
+
+.PHONY: postgres createdb dropdb migrateup migratedown migrateup1 migratedown1 sqlc server mock proto evans
---
+++ gapi/server.go
@@ -0,0 +1,31 @@
+package gapi
+
+import (
+	db "bankapp/db/sqlc"
+	"bankapp/pb"
+	"bankapp/token"
+	"bankapp/util"
+	"fmt"
+)
+
+// Server serves gRPC requests for banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+	return server, nil
+}
