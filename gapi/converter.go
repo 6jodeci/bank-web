663e97cfd1439b7d112acfa8ae4982d9b8905ca2
6jodeci <egor0263s@gmail.com>
2022-10-01 20:20:39 +0500

    Implement gRPC API to create and login users

---
+++ gapi/converter.go
@@ -0,0 +1,18 @@
+package gapi
+
+import (
+	db "bankapp/db/sqlc"
+	"bankapp/pb"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
