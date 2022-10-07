40883b9014c27dd27f18dd3434ee1f0c8920b9f9
6jodeci <egor0263s@gmail.com>
2022-10-07 19:21:52 +0500

    migrate

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 set -e
-
-echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+# echo "run db migration"
+# /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "start the app"
 exec "$@"
