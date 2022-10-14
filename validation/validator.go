8703147846afc0582d49d9a2e28aae08d66e1a10
6jodeci <egor0263s@gmail.com>
2022-10-15 01:18:19 +0500

    gRPC update API with optional parameters

--- validation/validator.go
+++ validation/validator.go
@@ -8,7 +8,7 @@ import (
 
 var(
 	isValidUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
-	isValidFullname = regexp.MustCompile(`^[a-zA-Z_\\s]+$`).MatchString
+	isValidFullname = regexp.MustCompile(`^[a-zA-Z\s]+$`).MatchString
 )
 
 func ValidateString(value string, minLenght, maxLenght int) error {
